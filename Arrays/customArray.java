ea5106da8968e4be95691422f01c06c1fa3cc875
nolan550 <nolandavis457@gmail.com>
2025-10-21 06:00:51 +0300

    customArray in java

---
+++ Arrays/customArray.java
@@ -0,0 +1,70 @@
+
+public class customArray{
+
+    private int [] data;
+    private int size;
+
+    public customArray(int capacity){
+        data = new int[capacity];
+        size = 0;
+    }
+
+
+    public void add(int value){
+        if(size == data.length){
+            resize();
+        }
+        data[size] = value;
+        size++;
+    }
+
+    private void resize(){
+        int[] newData = new int[data.length * 2];
+        for(int i = 0; i < data.length; i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public int get(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index is out of bound");
+        }
+        return data[index];
+    }
+
+    public void print(){
+        for(int i = 0; i < size; i++){
+            System.out.println(data[i]);
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public static void main(String[] args){
+        customArray array1 = new customArray(7);
+
+       
+        array1.add(7);
+        array1.add(23);
+        array1.add(21);
+        array1.add(45);
+        array1.add(56);
+        array1.add(87);
+        array1.add(76);
+        array1.add(743);
+        array1.add(23);
+        array1.add(71);
+        array1.add(55);
+        array1.add(90);
+        array1.add(27);
+        array1.add(77);
+        array1.add(222);
+
+        array1.print();
+        System.out.println("The size of an array is:" + array1.size());
+        System.out.println( array1.get(30));
+    }
+}
