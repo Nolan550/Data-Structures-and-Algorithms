8573e4d9702fab0229df7879932157869a87be83
nolan550 <nolandavis457@gmail.com>
2025-11-25 09:00:29 +0300

    updated linkedlist

--- LinkedLists/singly_linkedlist.py
+++ LinkedLists/singly_linkedlist.py
@@ -1,26 +1,80 @@
 class Node:
+
     def __init__(self, data):
         self.data = data
         self.next = None
 
-n1 = Node(11)
-n2 = Node(12)
-n3 = Node(13)
-n4 = Node(14)
-n5 = Node(15)
-n6 = Node(16)
+class singlyLinkedList:
+    def __init__(self):
+        self.head = None
 
-n1.next = n2
-n2.next = n3
-n3.next = n4 
-n4.next = n5
-n5.next = n6
+    def append(self,data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            return
+        
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = newNode
+    
+    def prepend(self, data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
 
-head = n1
+    def deleteValue(self, data):
+        if self.head is None:
+            return
+        
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next is not None and current.next.data != data:
+            current = current.next
 
-current = head
-while current:
-    print(current.data, end="->")
-    current= current.next
-print("None")
+        if current.next is not None:
 
+            current.next = current.next.next
+
+    def displayList(self):
+        if self.head is None:
+            print("The list is empty")
+            return
+        current = self.head
+        print("The linked list is: ")
+        while current:
+            print(current.data, end="_>")
+            current = current.next
+        print("NULL")
+
+    def search(self,data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+
+if __name__ == "__main__":
+    list = singlyLinkedList()
+    list.append(11)
+    list.append(12)
+    list.append(13)
+    list.append(14)
+    list.append(15)
+    list.append(16)
+
+    list.displayList()
+
+    print(list.search(13))
+    print(list.search(25))
+
+    list.prepend(10)
+    list.displayList()
+    list.deleteValue(14)
+    print("After deleting 14:")
+    list.displayList()
